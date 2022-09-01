588a0821b63c9119684059e35982dc862fd97f46
Yohannes Teshome <83913756+John-droid-dotcom@users.noreply.github.com>
2022-09-01 22:31:48 +0300

    Create powerOfThree.js

---
+++ powerOfThree.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+    if(n == 0){
+        return false;
+    }
+    else if(n == 1){
+        return true;
+    }
+    else if(n%3 != 0){
+        return false;
+    }
+    
+    return isPowerOfThree(n/3);
+};
