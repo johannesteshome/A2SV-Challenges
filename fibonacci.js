9148747f06c86606b37aeb1bd803b93c8bc655cc
Yohannes Teshome <83913756+John-droid-dotcom@users.noreply.github.com>
2022-09-01 22:43:50 +0300

    Create fibonacci.js

---
+++ fibonacci.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n) {
+    if(n==0){
+        return 0;
+    }
+    else if(n == 1){
+        return 1;
+    }
+    else if(n > 1){
+        return fib(n-1) + fib(n-2);
+    }
+};
