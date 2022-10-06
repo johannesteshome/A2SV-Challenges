2e71eaef1c3938e54e4f649145a9ca8013d5942a
Yohannes Teshome <83913756+John-droid-dotcom@users.noreply.github.com>
2022-10-06 21:37:05 +0300

    Update rangeSumQuery2D.java

--- rangeSumQuery2D.java
+++ rangeSumQuery2D.java
@@ -14,12 +14,9 @@ class NumMatrix {
             int prefix = 0;
             
             for(int j=0; j<matrix[i].length; j++){
-               
                     prefix += matrix[i][j];
                     int above = sumMatrix[i][j+1];
                     sumMatrix[i+1][j+1] = prefix + above;
-            
-                
             }
         }
         System.out.println(Arrays.deepToString(sumMatrix));
@@ -32,8 +29,8 @@ class NumMatrix {
         int bottomRight = sumMatrix[row2][col2];
         int above = sumMatrix[row1-1][col2];
         int left = sumMatrix[row2][col1-1];
-        int topLeft = sumMatrix[row1][col1];
-        
+        int topLeft = sumMatrix[row1-1][col1-1];
+                
         return (bottomRight - above - left) + topLeft;
     }
 }
