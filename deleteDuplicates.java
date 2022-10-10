c857405656dce97289526275a938990bb54931a0
Yohannes Teshome <83913756+John-droid-dotcom@users.noreply.github.com>
2022-10-10 11:20:00 +0300

    Create deleteDuplicates.java

---
+++ deleteDuplicates.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+  public ListNode deleteDuplicates(ListNode head) {
+    ListNode dummy = new ListNode(0, head);
+    ListNode prev = dummy;
+
+    while (head != null) {
+      while (head.next != null && head.val == head.next.val)
+        head = head.next;
+      if (prev.next == head)
+        prev = prev.next;
+      else
+        prev.next = head.next;
+      head = head.next;
+    }
+
+    return dummy.next;
+  }
+}
